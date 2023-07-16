d047c7ce4d653c9fd868e0f18047e11a42057b5d
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2023-07-16 18:52:58 -0300

    login

---
+++ Psicologia-Web/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Psicologia Web</title>
+</head>
+<body>
+
+<div class="logo">
+<!--<img src="{{ asset('storage/logo.png') }}" alt="Imagem">-->
+Teste
+</div>
+    
+</body>
+</html>
