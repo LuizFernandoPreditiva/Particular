d3085624e71867171ec09649c4415afc12e8e3cb
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2023-08-14 21:14:39 -0300

    alteracoes

--- Psicologia-Web/resources/views/index.blade.php
+++ Psicologia-Web/resources/views/index.blade.php
@@ -3,15 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('storage/css/style.css') }}">
     <title>Psicologia Web</title>
 </head>
 <body>
 
-<div class="logo">
-<!--<img src="{{ asset('storage/logo.png') }}" alt="Imagem">-->
-Teste
+<div class="menu">
+    <h1>Menu</h1>
 </div>
-    
+
+<div class="logo">
+    <img src="{{ asset('storage/Logo.png') }}" alt="Imagem">
+</div>
+
+<div class="texto">
+    <p>Texto</p>
+</div>
+
 </body>
 </html>
