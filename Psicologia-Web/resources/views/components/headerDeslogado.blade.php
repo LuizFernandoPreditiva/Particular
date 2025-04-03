1f33f5ea56ba04cf8e7a25b2c506970d42832b29
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-04-03 00:39:34 -0300

    correcoes do menu
    correcoes de CSS menu

--- Psicologia-Web/resources/views/components/headerDeslogado.blade.php
+++ Psicologia-Web/resources/views/components/headerDeslogado.blade.php
@@ -1,31 +1,20 @@
-<!--    @component("components.menulogin")
-    @endcomponent -->
 
 <header class="container">
-    <div class="navbar">
-        <div class="logo">
-            <a href=""><img src="{{ asset('images/logo.png') }}" alt=""></a>
+    <nav class="navbar">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="logo">
+        <ul class="nav-links">
+            <li><a href="#" class="active">Página Inicial</a></li>
+            <li><a href="#">Sobre Mim</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+        <div class="actions">
+            <a href="#" class="btn-agendar">AGENDAR SESSÃO</a>
+            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Login</a>
         </div>
-        <div class="menu-opener">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    </div>
-        <nav>
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-            Login</a>
-        <a href="" class="button">Contato</a>
     </nav>
 </header>
 
-<div class="container-menuBarDeslogado"> 
-    <div class="menuBarDeslogado">
-        <a href="home">Home</a>
-        <a href="">Sobre</a>
-        <a href="">Contato</a>
-    </div>
-</div>
 
 
 
--- Psicologia-Web/resources/views/components/headerLogado.blade.php
+++ Psicologia-Web/resources/views/components/headerLogado.blade.php
@@ -1,50 +1,63 @@
-@component("components.menulogout")
-@endcomponent
+<header class="container">
 
-<div class="logologado">
-@component("components.logo")
-@endcomponent
-</div>
-
-<div class="containerMenuBarLogado">
-    <div class="menuBarLogado">
-        <a href="{{route('logado')}}">Inicio</a>
-    </div>
-
-    <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="{{route('clientes.index')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Pacientes
-        </a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="{{route('clientes.index')}}">Lista</a></li>
-            <li><a class="dropdown-item" href="{{route('clientes.create')}}">Novo</a></li>
-            <li><a class="dropdown-item" href="{{route('clientes.pesquisar')}}">Buscar</a></li>
-            <hr>
-            <li><a class="dropdown-item" href="{{route('clientes.ativo')}}">Atendimentos</a></li>
-            <li><a class="dropdown-item" href="{{route('clientes.alta')}}">Alta</a></li>
-            <li><a class="dropdown-item" href="{{route('clientes.inativo')}}">Desistencia</a></li>
+    <nav class="navbar">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="logo">
+        <ul class="nav-links">
+            <li>
+                <div class="menuBarLogado">
+                    <a href="{{route('logado')}}">Inicio</a>
+                </div>
+            </li>
+            <li>
+                <div class="dropdown">
+                    <a class="btn btn-secondary dropdown-toggle" href="{{route('clientes.index')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pacientes
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{route('clientes.index')}}">Lista</a></li>
+                        <li><a class="dropdown-item" href="{{route('clientes.create')}}">Novo</a></li>
+                        <li><a class="dropdown-item" href="{{route('clientes.pesquisar')}}">Buscar</a></li>
+                        <hr>
+                        <li><a class="dropdown-item" href="{{route('clientes.ativo')}}">Atendimentos</a></li>
+                        <li><a class="dropdown-item" href="{{route('clientes.alta')}}">Alta</a></li>
+                        <li><a class="dropdown-item" href="{{route('clientes.inativo')}}">Desistencia</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div class="dropdown">
+                    <a class="btn btn-secondary dropdown-toggle" href="{{route('pagamentos.pesquisar')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pagamentos
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{route('pagamentos.pesquisar')}}">Lancar</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div class="dropdown">
+                    <a class="btn btn-secondary dropdown-toggle" href="{{route('atendimentos.index')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Atendimentos
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{route('atendimentos.create')}}">Novo</a></li>
+                    </ul>
+                </div>
+            </li>      
         </ul>
-    </div>
-
-    <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="{{route('pagamentos.pesquisar')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Pagamentos
-        </a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="{{route('pagamentos.pesquisar')}}">Lancar</a></li>
-        </ul>
-    </div>
-
-    <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="{{route('atendimentos.index')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Atendimentos
-        </a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="{{route('atendimentos.create')}}">Novo</a></li>
-        </ul>
-    </div>
-
-</div>
+        <div class="menuLogin">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+        
+                <x-responsive-nav-link :href="route('logout')"
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-responsive-nav-link>
+            </form>
+        </div>
+    </nav>
+</header>
 
 
 <!-- jQuery -->
