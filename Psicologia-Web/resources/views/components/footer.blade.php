78eda41edff273fa3972dea4e56dcbe17ee8ee01
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-04-10 12:27:20 -0300

    Footer
    inicio do footer

--- Psicologia-Web/resources/views/components/footer.blade.php
+++ Psicologia-Web/resources/views/components/footer.blade.php
@@ -1,3 +1,29 @@
-<div class="footer">
-    Desenvolvimento: luiz.fernando@viannasempre.com.br
-</div>
+<section class="collab">
+    <p class="title">Tem interesse em fazer algum trabalho comigo?</p>
+    <p class="subtitle">Estou sempre aberto a discutir projetos e possíveis parcerias de trabalho.</p>
+    <a href="https://wa.me/+5532988097534" class="button">Começar uma conversa</a>
+</section>
+
+<footer>
+    <a href="">
+        <img class="logo" src="images/logo_white.png" alt="" />
+    </a>
+    <p class="title">Vivendo, aprendendo e melhorando um dia de cada vez.</p>
+
+    <div class="social">
+        <a href="https://www.instagram.com/luiz_fernando.11/">
+            <img src="images/social_ig.svg" alt="Instagram" title="Instagram"/>
+        </a>
+        <a href="https://www.linkedin.com/in/luiz-fernando-duarte-cabette-3a0ba7193/">
+            <img src="images/social_Linkedin.svg" alt="LinkedIn" title="LinkedIn"/>
+        </a>
+        <a href="mailto:luiz.fernando@viannasempre.com.br">
+            <img src="images/social_email.svg" alt="E-mail" title="E-mail"/>
+        </a>
+        <a href="https://wa.me/+5532988097534">
+            <img src="images/social_zap.svg" alt="WhatsApp" title="WhatsApp"/>
+        </a>
+    </div>
+    <p class="copyright">Criado por: Luiz Fernando Duarte Cabette.</p>
+    <p class="legal">CPF: 084.831.716-51</p>
+</footer>
