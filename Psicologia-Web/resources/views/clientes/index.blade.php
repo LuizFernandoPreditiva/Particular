748f3c947e8e2cf8635a0c5e049cc2d3d9c620c5
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-04-21 23:56:42 -0300

    tabelas
    css

--- Psicologia-Web/resources/views/clientes/index.blade.php
+++ Psicologia-Web/resources/views/clientes/index.blade.php
@@ -1,32 +1,34 @@
-@extends('layouts.principal')
+@extends('layouts.principalLogado')
 
 @section('main')
 
-    <table class="TabelaClientes">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Telefone</th>
-                <th>Registrado</th>
-                <th>Visualizar</th>
-                <th>Atendimentos</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach($clientes as $cl):
-            ?>
-            <tr>
-                <td><?= $cl->nome ?></td>
-                <td><?= $cl->telefone ?></td>
-                <td><?= date('d/m/Y', strtotime($cl->created_at)) ?></td>
-                <td><a href="{{route('clientes.show', $cl->id )}}">Visualizar</a></td>
-                <td><a href="{{route('atendimentos.registro', $cl )}}">Visualizar</a></td>
-            </tr>
-            <?php
-                endforeach;
-            ?>
-        </tbody>
-    </table>
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Telefone</th>
+                    <th>Registrado</th>
+                    <th>Visualizar</th>
+                    <th>Atendimentos</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($clientes as $cl):
+                ?>
+                <tr>
+                    <td><?= $cl->nome ?></td>
+                    <td><?= $cl->telefone ?></td>
+                    <td><?= date('d/m/Y', strtotime($cl->created_at)) ?></td>
+                    <td><a href="{{route('clientes.show', $cl->id )}}">Visualizar</a></td>
+                    <td><a href="{{route('atendimentos.registro', $cl )}}">Visualizar</a></td>
+                </tr>
+                <?php
+                    endforeach;
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 @endsection
