899a775c530b25039ba64d37b5820eda13596315
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-04-22 22:59:46 -0300

    logado
    backgroud da blade logado

--- Psicologia-Web/resources/views/components/footer.blade.php
+++ Psicologia-Web/resources/views/components/footer.blade.php
@@ -1,9 +1,3 @@
-<section class="collab">
-    <p class="title">Tem interesse em fazer algum trabalho comigo?</p>
-    <p class="subtitle">Estou sempre aberto a discutir projetos e possíveis parcerias de trabalho.</p>
-    <a href="https://wa.me/+5532988097534" class="button">Começar uma conversa</a>
-</section>
-
 <footer>
     <a href="https://luizfernando11.github.io/Portifolio/">
         <img class="logo" src="{{ asset('images/logo_white.png') }}" alt="Link portifolio" title="Portifolio" />
@@ -12,18 +6,24 @@
 
     <div class="social">
         <a href="https://www.instagram.com/luiz_fernando.11/">
-            <img src="images/social_ig.svg" alt="Instagram" title="Instagram"/>
+            <img src="{{ asset('images/social_ig.svg') }}" alt="Instagram" title="Instagram"/>
         </a>
         <a href="https://www.linkedin.com/in/luiz-fernando-duarte-cabette-3a0ba7193/">
-            <img src="images/social_Linkedin.svg" alt="LinkedIn" title="LinkedIn"/>
+            <img src="{{ asset('images/social_linkedin.svg') }}" alt="LinkedIn" title="LinkedIn"/>
         </a>
         <a href="mailto:luiz.fernando@viannasempre.com.br">
-            <img src="images/social_email.svg" alt="E-mail" title="E-mail"/>
+            <img src="{{ asset('images/social_email.svg') }}" alt="E-mail" title="E-mail"/>
         </a>
         <a href="https://wa.me/+5532988097534">
-            <img src="images/social_zap.svg" alt="WhatsApp" title="WhatsApp"/>
+            <img src="{{ asset('images/social_zap.svg') }}" alt="WhatsApp" title="WhatsApp"/>
         </a>
     </div>
     <p class="copyright">Criado por: Luiz Fernando Duarte Cabette.</p>
     <p class="legal">CPF: 084.831.716-51</p>
+
+    <section class="collab">
+        <p class="title">Tem interesse em fazer algum trabalho comigo?</p>
+        <p class="subtitle">Estou sempre aberto a discutir projetos e possíveis parcerias de trabalho.</p>
+        <a href="https://wa.me/+5532988097534" class="button">Começar uma conversa</a>
+    </section>
 </footer>
--- Psicologia-Web/resources/views/logado.blade.php
+++ Psicologia-Web/resources/views/logado.blade.php
@@ -2,5 +2,11 @@
 
 @section('main')
 
+<div class="foto-bg">
+    <div class="conteudo-texto">
+        <h1>Natália Cabette Lanini Duarte</h1>
+        <p>Psicologa</p>
+    </div>
+</div>
 
 @endsection
