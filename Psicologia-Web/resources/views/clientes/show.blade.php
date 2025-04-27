fbc11522b5d8084e5421c29b374c8cf25eac7a27
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-04-27 15:54:11 -0300

    show css
    css da page show cliente

--- Psicologia-Web/resources/views/clientes/show.blade.php
+++ Psicologia-Web/resources/views/clientes/show.blade.php
@@ -2,31 +2,34 @@
 
 @section('main')
 
-    <h1>Cliente:</h1>
-    ID: {{$cliente->id}}<br>
-    Nome: {{$cliente->nome}}<br>
-    CPF: {{$cliente->cpf}}<br>
-    Telefone: {{$cliente->telefone}}<br>
-    Endereco: {{$cliente->endereco}}<br>
-    Cidade: {{$cliente->cidade}}<br>
-    Estado: {{$cliente->estado}}<br>
-    status: {{$cliente->status}}<br>
-    Atendimentos: {{$cliente->atendimentos}}<br>
-    Faltas: {{$cliente->faltas}}<br>
-    Saldo: {{$cliente->saldo}}<br>
+    <div class="exibir-cliente">
+        <h1>Cliente:</h1>
+        <p>
+            ID: {{$cliente->id}}<br>
+            Nome: {{$cliente->nome}}<br>
+            CPF: {{$cliente->cpf}}<br>
+            Telefone: {{$cliente->telefone}}<br>
+            Endereco: {{$cliente->endereco}}<br>
+            Cidade: {{$cliente->cidade}}<br>
+            Estado: {{$cliente->estado}}<br>
+            status: {{$cliente->status}}<br>
+            Atendimentos: {{$cliente->atendimentos}}<br>
+            Faltas: {{$cliente->faltas}}<br>
+            Saldo: {{$cliente->saldo}}<br>
+        </p>
 
-    <form id="form-delete-{{ $cliente->id }}" action="{{route('clientes.destroy', $cliente)}}" method="post">
-        <a class="btn btn-primary" href="{{route('clientes.edit', $cliente->id )}}" role="button">Alterar</a>
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-primary" onclick="confirmDelete({{ $cliente->id }})">Deletar</button>
-        <a class="btn btn-primary" href="{{route('pagamentos.novo', ['id' => $cliente->id ] )}}" role="button">Novo Pagamento</a>
-        <a class="btn btn-primary" href="{{route('pagamentos.historico', $cliente )}}" role="button">Financeiro</a>
-        <a class="btn btn-primary" href="{{route('atendimentos.registro', $cliente )}}" role="button">Atendimentos</a>
-    </form>
-
-    <script type="text/javascript" src="{{ asset('js/delete.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    
+        <form id="form-delete-{{ $cliente->id }}" action="{{route('clientes.destroy', $cliente)}}" method="post">
+            <a class="btn btn-primary" href="{{route('clientes.edit', $cliente->id )}}" role="button">Alterar</a>
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-primary" onclick="confirmDelete({{ $cliente->id }})">Deletar</button>
+            <a class="btn btn-primary" href="{{route('pagamentos.novo', ['id' => $cliente->id ] )}}" role="button">Novo Pagamento</a>
+            <a class="btn btn-primary" href="{{route('pagamentos.historico', $cliente )}}" role="button">Financeiro</a>
+            <a class="btn btn-primary" href="{{route('atendimentos.registro', $cliente )}}" role="button">Atendimentos</a>
+        </form>
+    </div>
 
 @endsection
+
+<script type="text/javascript" src="{{ asset('js/delete.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
